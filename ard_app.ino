280b25643c12436c51472aa51a6a7164f0f4067e
Casa Altair <78558468+casa-altair@users.noreply.github.com>
2024-04-14 11:53:34 +0530

    Update ard_app.ino

--- ard_app.ino
+++ ard_app.ino
@@ -1,22 +1,32 @@
 #include <MQ135.h>
 #include <DHT.h>
+
 #define PIN_MQ135 A2
 #define DHTPIN 2
 #define DHTTYPE DHT11
-MQ135 mq135_sensor(PIN_MQ135);
 DHT dht(DHTPIN, DHTTYPE);
+
+MQ135 mq135_sensor(PIN_MQ135);
+
 float temperature, humidity;
 void setup() {
   Serial.begin(9600);
   dht.begin();
+  pinMode(7, OUTPUT);
+  digitalWrite(7, HIGH);
 }
 void loop() {
   humidity = dht.readHumidity();
   temperature = dht.readTemperature();
+
   if (isnan(humidity) || isnan(temperature)) {
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
+  digitalWrite(7, ((temperature < 35.0) ? LOW : HIGH));
+  (temperature < 35.0) ? Serial.println("LOW") : Serial.println("HIGH");
+ 
+  
   float correctedPPM = mq135_sensor.getCorrectedPPM(temperature, humidity);
   Serial.print(temperature);
   Serial.print(",");
